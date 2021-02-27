bd127e69dbe6d3fa33558b9ac0bbeee60e723e9c
Your Name Comes HereMOhamed <mohamedatta447@gmail.com>
2021-02-27 09:15:57 +0200

    finishing design

---
+++ comps/Navbar.js
@@ -0,0 +1,80 @@
+import {
+  Button,
+  Box,
+  Flex,
+  MenuItem,
+  Menu,
+  MenuButton,
+  MenuList,
+  Heading,
+  Spacer,
+  IconButton,
+  Input,
+  InputGroup,
+  InputLeftAddon,
+} from "@chakra-ui/react";
+import { HamburgerIcon, SearchIcon } from "@chakra-ui/icons";
+
+const Navbar = () => {
+  return (
+    <nav bg="#00a67d">
+      <Flex bg="#00a67d" p={3} px={{ lg: 10 }} mb={5}>
+        <Heading color="white" size="md" my="auto">
+          newsbuzz
+        </Heading>
+
+        <Spacer />
+
+        <Box display={{ lg: "none", base: "block" }}>
+          <InputGroup bg="white" borderRadius={10}>
+            <InputLeftAddon children={<SearchIcon color="#00a67d" />} />
+            <Input type="text" placeholder="Search language or item" />
+          </InputGroup>
+        </Box>
+
+        <Spacer />
+
+        <Box display={{ lg: "none", base: "block" }}>
+          <Menu>
+            <MenuButton
+              as={IconButton}
+              aria-label="Options"
+              icon={<HamburgerIcon />}
+              size="md"
+              variant="outline"
+              border={0}
+              color="#fff"
+            />
+            <MenuList>
+              <MenuItem>Login</MenuItem>
+              <MenuItem>Register</MenuItem>
+            </MenuList>
+          </Menu>
+        </Box>
+
+        <Flex display={{ lg: "block", base: "none" }}>
+          <Button
+            variant="outline"
+            colorScheme="green"
+            borderColor="White"
+            color="white"
+            borderRadius="20px"
+            mr={1}
+          >
+            LOGIN
+          </Button>
+          <Button
+            // variant="outline"
+            bg="#f5f6f7"
+            color="green.600"
+            borderRadius="20px"
+          >
+            Regiter
+          </Button>
+        </Flex>
+      </Flex>
+    </nav>
+  );
+};
+
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,43 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { Box, Container, Flex } from "@chakra-ui/react";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Navbar from "../comps/Navbar";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>Sport News | Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+      <Navbar />
+      <Container backgroundColor="#f5f5f5">
+        <Flex>
+          <Box
+            display={{ lg: "block", base: "none" }}
+            pos="fixed"
+            height="100%"
+            w="200px"
+            left="20"
+            bg="whatsapp.100"
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
+            Hello
+          </Box>
+          <Box flex="1" bg="whatsapp.100">
+            Hello
+          </Box>
+          <Box
+            display={{ lg: "block", base: "none" }}
+            pos="fixed"
+            height="100%"
+            w="200px"
+            right="20"
+            bg="whatsapp.100"
           >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+            Hello
+          </Box>
+        </Flex>
+      </Container>
+    </>
+  );
 }
